232896d6386b7564770fb579db8c8f591a5977a9
paulinefuzier <paulinefuzier@hotmail.fr>
2017-11-21 09:16:06 +0100

    add loginServiceTest delete AppTest

---
+++ src/test/java/Geco/LoginServiceTest.java
@@ -0,0 +1,12 @@
+package Geco;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
